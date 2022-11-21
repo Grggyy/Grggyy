cf3dbbed0e5260a2264f39fedc89e96de092d0d6
Grggyy <116067143+Grggyy@users.noreply.github.com>
2022-11-21 17:37:49 +0800

    Add files via upload

---
+++ validation form/script.js
@@ -0,0 +1,19 @@
+function alphaOnly(event) {
+
+  var key;
+
+  if (window.event) {
+    key = window.event.key;
+  } else if (e) {
+    key = e.which;
+  }
+  //var key = window.event.key || event.key;
+  alert(key.value);
+  return ((key >= 65 && key <= 90) || (key >= 95 && key <= 122));
+
+}
+
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', () => {
+  alert('You submitted a registration form');
+});
